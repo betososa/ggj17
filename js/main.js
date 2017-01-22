a3017fa8175a27a0bfc060c32e559d7f967761ea
Beto Sosa <betososah@gmail.com>
2017-01-21 18:24:16 -0600

    integrando slides a canvas

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
 (function () {
+
+
+
+    
     var meter = null;
     var ecg = null;
 
     $(function () {
-        ecg = new ECG('ecg');
-        ecg.init();
-        var canvas = $('#meter')[0];
-        meter = new Meter(canvas, 500, undefined, badStroke);
-        meter.startMeter();
+        var mySwiper = new Swiper ('.swiper-container', {
+            direction: 'horizontal',
+            loop: false,
+            keyboardControl: true,
+            onSlideChangeEnd: function(e) {
+                console.log(e);
+                if (e.activeIndex === 3) {
+                    ecg = new ECG('ecg');
+                    ecg.init();
+                    var canvas = $('#meter')[0];
+                    meter = new Meter(canvas, 500, undefined, badStroke);
+                    meter.startMeter();
+                }
+            }
+        });
     });
 
     function badStroke() {
