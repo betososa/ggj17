4ee40918da3efb1a0847a35dbe623dfd5d9d5a74
Beto Sosa <betososah@gmail.com>
2017-01-22 10:43:11 -0600

    Imagenes en historia y splash

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
 (function () {
     var meter = null;
     ecg = null;
-
+    var randomName = [
+        "Chuck Norris",
+        "James Bond",
+        "Princess Leia",
+        "Katniss Everdeen",
+        "Yoda"
+    ];
     $(function () {
+        $('#credits').click(function() {
+            crate.init({
+                setTitle: 'Globant Mx',
+                setBody: '<img id="splash" src="../img/ggjSplash.jpg" />',
+                closeActions: {
+                    button: true,
+                    clickIn: true,
+                    clickOut: true
+                }
+            })
+        });
         var mySwiper = new Swiper ('.swiper-container', {
             direction: 'horizontal',
             loop: false,
             keyboardControl: true,
             onSlideChangeEnd: function(e) {
-                console.log(e);
                 if (e.activeIndex === 3) {
                     if (!ecg) {
+                        var name = $('#userName').val() || 
+                            randomName[Math.floor(Math.random() * randomName.length)];
+                        $('#user').text(name);
                         window.ecg = new ECG('ecg');
                         ecg.init();
                         var canvas = $('#meter')[0];
